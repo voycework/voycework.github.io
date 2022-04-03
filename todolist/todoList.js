09479de9a7073c01f1a37db75c229c391a9fef4d
MfundoDev <100078136+MfundoDev@users.noreply.github.com>
2022-04-03 14:36:35 +0200

    To do List

---
+++ todolist/todoList.js
@@ -0,0 +1,9 @@
+
+
+function todoList() {
+    var item = document.getElementById("todoInput").value
+    var text = document.createTextNode(item)
+    var newItem = document.createElement("li")
+    newItem.appendChild(text)
+    document.getElementById('todoList').appendChild(newItem)
+}
