3fe680113974bf13469317ec9ee465146c10fc7c
Mfundovoyce <mfundomatsha@gmail.com>
2022-08-24 23:47:54 +0200

    aura site

---
+++ AURA SIte/aura.js
@@ -0,0 +1,14 @@
+// js for toggle Menu
+
+let navlinks = document.getElementById("nav-link");
+
+function showMenu(){
+    navlinks.style.right ="0";
+}
+function hideMenu(){
+    navlinks.style.right ="0";
+}
+
+
+
+navlinks.eventListener()
