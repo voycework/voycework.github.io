b628f6143f4eb8b4f9c0acf2742fd5341535ca77
Mfundovoyce <mfundomatsha@gmail.com>
2022-08-28 21:36:15 +0200

    navlink update

--- AURA SIte/aura.js
+++ AURA SIte/aura.js
@@ -1,14 +1,24 @@
 // js for toggle Menu
 
+let counter = 0;
+
+document.getElementById("bars").addEventListener( "click", function(){
+
 let navlinks = document.getElementById("nav-link");
+let countDisp = document.getElementById("counter");
 
-function showMenu(){
-    navlinks.style.right ="0";
+
+
+    if (disp() % 2 !== 0){
+        navlinks.style.display="block";
+
+    }else{
+        navlinks.style.display="none";
+    }
+    counter.innerHTML = "disp()"; 
+})
+
+function disp(){
+    counter = counter + 1;
+    return counter;
 }
-function hideMenu(){
-    navlinks.style.right ="0";
-}
-
-
-
-navlinks.eventListener()
